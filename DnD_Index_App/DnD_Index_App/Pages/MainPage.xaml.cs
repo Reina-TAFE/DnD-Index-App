13e4d7567201a1d6e2ba99b0c2cfc72f5934a73e
ReinaTAFE <20066312@tafe.wa.edu.au>
2026-03-04 17:22:26 +0800

    feat: add home page

--- DnD_Index_App/DnD_Index_App/Pages/MainPage.xaml.cs
+++ DnD_Index_App/DnD_Index_App/Pages/MainPage.xaml.cs
@@ -11,14 +11,12 @@
 
         private void OnCounterClicked(object? sender, EventArgs e)
         {
-            count++;
+            return;
+        }
 
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
-
-            SemanticScreenReader.Announce(CounterBtn.Text);
+        private void SettingsBtnClicked(object? sender, EventArgs e)
+        {
+            return;
         }
     }
 }
