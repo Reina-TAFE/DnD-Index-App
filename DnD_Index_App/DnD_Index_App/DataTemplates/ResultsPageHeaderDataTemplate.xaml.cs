eccca89a3a0ae6b64ac7c308afc8d53c47fc8f0a
Reina <20066312@tafe.wa.edu.au>
2026-04-21 11:42:11 +0800

    feat: add MVVM structure

---
+++ DnD_Index_App/DnD_Index_App/DataTemplates/ResultsPageHeaderDataTemplate.xaml.cs
@@ -0,0 +1,9 @@
+namespace DnD_Index_App.DataTemplates;
+
+public partial class ResultsPageHeaderDataTemplate : ContentView
+{
+	public ResultsPageHeaderDataTemplate()
+	{
+		InitializeComponent();
+	}
+}
---
+++ DnD_Index_App/DnD_Index_App/ViewModels/ResultsPageViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DnD_Index_App.ViewModels
+{
+    public class ResultsPageViewModel
+    {
+    }
+}
