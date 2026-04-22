f6c799c103b4cff6e5978f7021b94fddfc9bd14a
ReinaTAFE <20066312@tafe.wa.edu.au>
2026-04-22 16:57:24 +0800

    feat: convert pages to use theme preference for colours

--- DnD_Index_App/DnD_Index_App/Pages/SettingsPage.xaml.cs
+++ DnD_Index_App/DnD_Index_App/Pages/SettingsPage.xaml.cs
@@ -6,19 +6,19 @@ namespace DnD_Index_App.Pages;
 public partial class SettingsPage : ContentPage
 {
     //public PreferenceManager AppPreferenceManager { get; set; }
-    public string BackgroundColour { get { return Preferences.Get("BackgroundColour", "#582929"); } set; }
-    public string SectionColour { get { return Preferences.Get("SectionColour", "#FFFFFF"); } set; }
-    public string ButtonColour { get { return Preferences.Get("ButtonColour", "#FFFFFF"); } set; }
-    public string TextColour { get { return Preferences.Get("TextColour", "#FFFFFF"); } set; }
-    public string TitleColour { get { return Preferences.Get("TitleColour", "#FFFFFF"); } set; }
-    public string NavColour { get { return Preferences.Get("NavColour", "#FFFFFF"); } set; }
+    //public string BackgroundColour { get { return Preferences.Get("BackgroundColour", "#582929"); } set; } = Preferences.Get("BackgroundColour", "#582929");
+    //public string SectionColour { get { return Preferences.Get("SectionColour", "#FFFFFF"); } set; } = Preferences.Get("SectionColour", "#FFFFFF");
+    //public string ButtonColour { get { return Preferences.Get("ButtonColour", "#FFFFFF"); } set; } = Preferences.Get("ButtonColour", "#FFFFFF");
+    //public string TextColour { get { return Preferences.Get("TextColour", "#FFFFFF"); } set; } = Preferences.Get("TextColour", "#FFFFFF");
+    //public string TitleColour { get { return Preferences.Get("TitleColour", "#FFFFFF"); } set; } = Preferences.Get("TitleColour", "#FFFFFF");
+    //public string NavColour { get { return Preferences.Get("NavColour", "#FFFFFF"); } set; } = Preferences.Get("NavColour", "#FFFFFF");
 
 
 	public SettingsPage()
 	{
-        PreferenceManager.SetTheme(PreferenceManager.Themes[0]);
 		InitializeComponent();
-	}
+        PreferenceManager.UpdateResourceColours();
+    }
 
     private async void BackBtn_Tapped(object sender, TappedEventArgs e)
     {
@@ -27,6 +27,14 @@ public partial class SettingsPage : ContentPage
 
     private void DarkModeSwitch_Toggled(object sender, ToggledEventArgs e)
     {
-        PreferenceManager.SetTheme(PreferenceManager.Themes[(e.Value == true) ? 1 : 0]);
+        if (e.Value == true)
+        {
+            PreferenceManager.SetCurrentTheme("Dark Mode");
+        }
+        else
+        {
+            PreferenceManager.SetCurrentTheme("Light Mode");
+        }
+
     }
 }
