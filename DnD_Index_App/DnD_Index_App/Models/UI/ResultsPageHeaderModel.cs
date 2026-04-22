ff52e1ea7981569d4126e849bde135b4fc37e5ae
Reina <20066312@tafe.wa.edu.au>
2026-04-22 22:50:11 +0800

    feat: add UI component model ResultsPageHeaderModel

---
+++ DnD_Index_App/DnD_Index_App/Models/UI/ResultsPageHeaderModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DnD_Index_App.Models.UI
+{
+    public class ResultsPageHeaderModel
+    {
+        private string? _titleText;
+        public string TitleText
+        {
+            get { return (_titleText != null) ? _titleText : ""; }
+            set { _titleText = value; }
+        }
+
+        private string? _subTitleText;
+        public string SubTitleText
+        {
+            get { return (_subTitleText != null) ? _subTitleText : ""; }
+            set { _subTitleText = value; }
+        }
+
+        private Image? _icon;
+        public Image Icon
+        {
+            get { return (_icon != null) ? _icon : new Image(); }
+            set { _icon = value; }
+        }
+    }
+}
