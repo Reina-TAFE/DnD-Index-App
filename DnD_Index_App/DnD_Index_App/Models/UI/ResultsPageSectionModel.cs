edbd145479b108945b5f7dfbb1a915370a41d4e1
Reina <20066312@tafe.wa.edu.au>
2026-04-22 22:50:44 +0800

    feat: add UI component model ResultsPageSectionModel

---
+++ DnD_Index_App/DnD_Index_App/Models/UI/ResultsPageSectionModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DnD_Index_App.Models.UI
+{
+    public class ResultsPageSectionModel
+    {
+        private string? _sectionType;
+        public string SectionType // Determines how the ContentViewModel will formot the content
+        {
+            get { return (_sectionType != null) ? _sectionType : "standard"; }
+            set { _sectionType = value; }
+        }
+        private string? _sectionTitle;
+        public string SectionTitle
+        {
+            get { return (_sectionTitle != null) ? _sectionTitle : ""; }
+            set { _sectionTitle = value; }
+        }
+
+        private Dictionary<string, object>? _sectionContent;
+        public Dictionary<string, object> SectionContent
+        {
+            get { return (_sectionContent != null) ? _sectionContent : new Dictionary<string, object>(); }
+            set { _sectionContent = value; }
+        }
+
+        public ResultsPageSectionModel(string? sectionTitle, Dictionary<string, object>? sectionContent)
+        {
+            SectionTitle = sectionTitle;
+            SectionContent = sectionContent;
+        }
+
+        public object ToObject()
+        {
+            return this as object;
+        }
+    }
+}
