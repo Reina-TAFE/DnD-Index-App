b59b493c1442bb9e3a11eff467072494a763cdc9
Reina <20066312@tafe.wa.edu.au>
2026-04-28 20:50:36 +0800

    feat: add nav bar component view model

---
+++ DnD_Index_App/DnD_Index_App/Models/UI/NavBarModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DnD_Index_App.Models.UI
+{
+    public static class NavBarModel
+    {
+        public static List<ImageButton> NavBarButtons = new List<ImageButton>()
+        {
+            new ImageButton()
+            {
+                Source = "/Assets/Icons/spellbook.png",
+                GestureRecognizers = { new TapGestureRecognizer() { Command = new Command(() => Shell.Current.GoToAsync("SearchPage", App.PageQueryOptions["SpellSearchPage"])) } },
+            },
+            new ImageButton()
+            {
+                Source = "/Assets/Icons/paladin.png",
+                GestureRecognizers = { new TapGestureRecognizer() { Command = new Command(() => Shell.Current.GoToAsync("SearchPage", App.PageQueryOptions["ClassesSearchPage"])) } },
+            },
+            new ImageButton()
+            {
+                Source = "/Assets/Icons/fighter_symbol.png",
+                GestureRecognizers = { new TapGestureRecognizer() { Command = new Command(() => Shell.Current.GoToAsync("SearchPage", App.PageQueryOptions["EquipmentSearchPage"])) } },
+            },
+            new ImageButton()
+            {
+                Source = "/Assets/Icons/scroll.png",
+                GestureRecognizers = { new TapGestureRecognizer() { Command = new Command(() => Shell.Current.GoToAsync("SearchPage", App.PageQueryOptions["SpellsSearchPage"])) } },
+            }
+        };
+    }
+}
