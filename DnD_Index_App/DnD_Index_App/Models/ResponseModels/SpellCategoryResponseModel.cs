2490b18099a02679ed184b58a91f66f97bed9aed
Reina <20066312@tafe.wa.edu.au>
2026-04-28 20:32:59 +0800

    feat: add response model for spell Categories

---
+++ DnD_Index_App/DnD_Index_App/Models/ResponseModels/SpellCategoryResponseModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using DnD_Index_App.Models;
+
+namespace DnD_Index_App.Models.ResponseModels
+{
+    public class SpellCategoryResponseModel
+    {
+        public int count {  get; set; }
+        public List<Result> results { get; set; }
+
+        public CategoryList ToModel()
+        {
+            List<SearchCatagory> newCategories = results.Select(r => new SearchCatagory(r.name, $"level {r.level} spells", r.index, r.url)).ToList();
+            return new CategoryList(count, newCategories);
+        }
+    }
+
+    public class Result
+    {
+        public string index { get; set; }
+        public string name { get; set; }
+        public int level { get; set; }
+        public string url { get; set; }
+    }
+}
