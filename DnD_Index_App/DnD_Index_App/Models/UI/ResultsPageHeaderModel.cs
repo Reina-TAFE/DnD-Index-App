f2ceb168cd5782cead4be18b599005f7917880eb
Reina <20066312@tafe.wa.edu.au>
2026-04-29 21:23:47 +0800

    chore: make header properties nullable

--- DnD_Index_App/DnD_Index_App/Models/UI/ResultsPageHeaderModel.cs
+++ DnD_Index_App/DnD_Index_App/Models/UI/ResultsPageHeaderModel.cs
@@ -7,24 +7,31 @@ namespace DnD_Index_App.Models.UI
     public class ResultsPageHeaderModel
     {
         private string? _titleText;
-        public string TitleText
+        public string? TitleText
         {
             get { return (_titleText != null) ? _titleText : ""; }
             set { _titleText = value; }
         }
 
         private string? _subTitleText;
-        public string SubTitleText
+        public string? SubTitleText
         {
             get { return (_subTitleText != null) ? _subTitleText : ""; }
             set { _subTitleText = value; }
         }
 
         private Image? _icon;
-        public Image Icon
+        public Image? Icon
         {
             get { return (_icon != null) ? _icon : new Image(); }
             set { _icon = value; }
+        }
+
+        public ResultsPageHeaderModel(string? titleText, string? subTitleText, Image? icon = null)
+        {
+            TitleText = titleText;
+            SubTitleText = subTitleText;
+            Icon = icon;
         }
     }
 }
