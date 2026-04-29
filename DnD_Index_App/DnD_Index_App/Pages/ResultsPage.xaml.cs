0ff150170e458de2503d40639474f8fd005edf68
Reina <20066312@tafe.wa.edu.au>
2026-04-29 19:23:39 +0800

    feat: build ResultsPage content from component ViewModels

--- DnD_Index_App/DnD_Index_App/Pages/ResultsPage.xaml.cs
+++ DnD_Index_App/DnD_Index_App/Pages/ResultsPage.xaml.cs
@@ -1,9 +1,12 @@
+using DnD_Index_App.ViewModels;
+
 namespace DnD_Index_App.Pages;
 
 public partial class ResultsPage : ContentPage
 {
-	public ResultsPage()
+	public ResultsPage(ResultsPageViewModel viewModel)
 	{
 		InitializeComponent();
+		ResultsPageWrapper.Content = viewModel.Content;
 	}
 }
--- DnD_Index_App/DnD_Index_App/ViewModels/ResultsPageViewModel.cs
+++ DnD_Index_App/DnD_Index_App/ViewModels/ResultsPageViewModel.cs
@@ -8,23 +8,41 @@ using DnD_Index_App.ViewModels.ResultsPageComponentModels;
 
 namespace DnD_Index_App.ViewModels
 {
-    public class ResultsPageViewModel
+    public class ResultsPageViewModel : ContentView
     {
-        public ResultsPageHeaderViewModel Header { get; set; }
-        public ResultsPageSectionViewModel Body { get; set; }
+        public ResultsPageHeaderViewModel? Header { get; set; }
+        public ResultsPageSectionViewModel? Body { get; set; }
         public List<ImageButton> NavBar = NavBarModel.NavBarButtons;
 
-        public ResultsPageViewModel() 
+        public ResultsPageViewModel(ResultsPageHeaderViewModel? header, ResultsPageSectionViewModel? body) 
         {
-            Content = new Grid
+            Header = header;
+            Body = body;
+            Grid PageWrapper = new Grid
+            {
+                    RowDefinitions = new RowDefinitionCollection
+                {
+                    new RowDefinition { Height = new GridLength(1, GridUnitType.Star) },
+                    new RowDefinition { Height = GridLength.Auto },
+                },
+            };
+            Grid ContentGrid = new Grid
             {
                 RowDefinitions = new RowDefinitionCollection
-            {
-                new RowDefinition { Height = new GridLength(1, GridUnitType.Star) },
-                new RowDefinition { Height = GridLength.Auto },
-            },
-
+                {
+                    new RowDefinition { Height = GridLength.Auto },
+                    new RowDefinition { Height = new GridLength(1, GridUnitType.Star) },
+                },
             };
+            ContentGrid.Add(Header, 0, 0);
+            ContentGrid.Add(Body, 0, 1);
+            ScrollView ContentScrollView = new ScrollView
+            {
+                Content = ContentGrid
+            };
+            PageWrapper.Add(ContentScrollView, 0, 0);
+            Content = PageWrapper;
+
         }
     }
 }
