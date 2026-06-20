df4c717d48becc6895180053a1483dd6a4fa63b9
Reina <20066312@tafe.wa.edu.au>
2026-06-20 15:00:34 +0800

    feat: add navigation to nav bar buttons MainPage

--- DnD_Index_App/DnD_Index_App/Pages/MainPage.xaml.cs
+++ DnD_Index_App/DnD_Index_App/Pages/MainPage.xaml.cs
@@ -46,5 +46,28 @@ namespace DnD_Index_App.Pages
             }
             await Shell.Current.GoToAsync("SearchPage", queryOptions);
         }
+
+        private async void NavSearchBtn_Tapped(object sender, TappedEventArgs e)
+        {
+            ImageButton searchPageOption = (ImageButton)sender;
+            IDictionary<string, object> queryOptions = new Dictionary<string, object>();
+            if (searchPageOption.StyleId == "NavSpellsBtn")
+            {
+                queryOptions = await App.PageQueryOptions("SpellSearchPage");
+            }
+            else if (searchPageOption.StyleId == "NavClassesBtn")
+            {
+                queryOptions = await App.PageQueryOptions("ClassesSearchPage");
+            }
+            else if (searchPageOption.StyleId == "NavEquipmentBtn")
+            {
+                queryOptions = await App.PageQueryOptions("EquipmentSearchPage");
+            }
+            else if (searchPageOption.StyleId == "NavRulesBtn")
+            {
+                queryOptions = await App.PageQueryOptions("RulesSearchPage");
+            }
+            await Shell.Current.GoToAsync("SearchPage", queryOptions);
+        }
     }
 }
